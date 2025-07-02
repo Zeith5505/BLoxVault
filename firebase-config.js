06b30ddd892b4ad523e53caac0b27529bcca0157
Zeith5505 <jhonlaurancecanedo@gmail.com>
2025-07-02 18:21:00 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAonI5aLim8uhpBpIbHTUbnWNnDO7uK1nA",
+  authDomain: "bloxvault-915ce.firebaseapp.com",
+  databaseURL: "https://bloxvault-915ce-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "bloxvault-915ce",
+  storageBucket: "bloxvault-915ce.appspot.com",
+  messagingSenderId: "476458271623",
+  appId: "1:476458271623:web:7adf42a42117a62b9b8922"
+};
+
+firebase.initializeApp(firebaseConfig);
